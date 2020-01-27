cf86442eab1058bac5175695614962ea8cc52c11
piggy-devil <muupaza@gmail.com>
2020-01-27 11:24:53 +0700

    handling invalid users

--- app/Http/Controllers/FriendRequestController.php
+++ app/Http/Controllers/FriendRequestController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Friend;
-use App\Http\Resources\Friend as FriendResource;
 use App\User;
+use App\Friend;
 use Illuminate\Http\Request;
+use App\Exceptions\UserNotFoundException;
+use App\Http\Resources\Friend as FriendResource;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class FriendRequestController extends Controller
 {
@@ -15,8 +17,12 @@ class FriendRequestController extends Controller
             'friend_id' => '',
         ]);
 
-        User::find($data['friend_id'])
-            ->friends()->attach(auth()->user());
+        try {
+            User::findOrFail($data['friend_id'])
+                ->friends()->attach(auth()->user());
+        } catch (ModelNotFoundException $e) {
+            throw new UserNotFoundException();
+        }
 
         return new FriendResource(
             Friend::where('user_id', auth()->user()->id)
