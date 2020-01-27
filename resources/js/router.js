315c894cbebee908bedc28492ab4f4d315df2f6f
piggy-devil <muupaza@gmail.com>
2020-01-27 11:11:23 +0700

    page titles

--- resources/js/router.js
+++ resources/js/router.js
@@ -12,11 +12,13 @@ export default new VueRouter({
     routes: [
         {
             path: '/', name: 'home', component: NewsFeed,
+            meta: { title: 'News Feed' }
         },
         {
             path: '/users/:userId',
             name: 'user.show',
             component: UserShow,
+            meta: { title: 'Profile' }
         }
     ]
 })
